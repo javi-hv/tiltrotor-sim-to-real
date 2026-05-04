1e07d68ccdec554d9e3314dc5a62b6eda7762430
javi-hv <javierandresh039@gmail.com>
2026-05-03 21:21:23 -0500

    Added docker file with qgc, gazebo and ardupilot

---
+++ docker/sitl/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Load environment
+# source ~/.profile
+
+# cd /ardupilot
+
+# echo "Starting ArduPilot SITL..."
+
+# Run SITL (start simple)
+
+# ./Tools/autotest/sim_vehicle.py -v ArduCopter -f gazebo-iris --console --map
+
+# Keep container running
+tail -f /dev/null
---
+++ docker/sitl/install.sh
@@ -0,0 +1,5 @@
+apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    git \
+    
